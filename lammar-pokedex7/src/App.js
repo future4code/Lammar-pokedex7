30d11ea8c010d884c9ae3adc6f7dc87197608be4
Caroline Marques <carolinemcs@hotmail.com>
2022-09-09 16:28:44 -0300

    Tentando resolver conflito

--- lammar-pokedex7/src/App.js
+++ lammar-pokedex7/src/App.js
@@ -1,17 +1,3 @@
-import React from 'react';
-// import { Rotas } from './Rotas/routers';
-import Home from "./pages/Home"; 
-import Pokedex from './pages/Pokedex';
-import PokedexDetails from './pages/PokedexDetails';
 
 
 
-function App() {
-  return (
-    <div>
-      <Home/>
-    </div>
-  );
-}
-
-export default App;
--- lammar-pokedex7/src/coordinator/coordinator.js
+++
@@ -1,8 +0,0 @@
-
-export const goToBack = (navigate) => {navigate(-1)};
-
-export const goToHome = (navigate) => {navigate("/")};
-
-export const goToAddPoke = (navigate) => {navigate("/addPoke")};
-
-export const goToPokedexDetails = (navigate) => {navigate("/pokeDetails")};
